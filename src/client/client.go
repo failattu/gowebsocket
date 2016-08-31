69d32da39431fd3cb4420b77a0b37eae757a011b
Failattu <kalle.sirkesalo@eficode.com>
2016-08-31 15:33:43 +0300

    ADDED message as parameter

--- src/client/client.go
+++ src/client/client.go
@@ -6,12 +6,12 @@ import (
     "golang.org/x/net/websocket"
 	"os"
 )
-func socketcalls(remoteurl string, originurl string){
+func socketcalls(remoteurl string, originurl string, message string){
 	ws, err := websocket.Dial(remoteurl, "", originurl)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := ws.Write([]byte("Red Hat  and lots of love for containers \n")); err != nil {
+	if _, err := ws.Write([]byte(message)); err != nil {
 		log.Fatal(err)
 	}
 	var msg = make([]byte, 512)
@@ -22,11 +22,12 @@ func socketcalls(remoteurl string, originurl string){
 	fmt.Printf("Received: %s.\n", msg[:n])
 }
 func main() {
-	if len(os.Args) > 2 {
+	if len(os.Args) > 3 {
 		origin := "http://localhost/"
 		baseurl := os.Args[1]
 		baseport := os.Args[2]
+		message := os.Args[3]
 		url := "ws://"+ baseurl+":"+baseport+"/echo"
-		socketcalls(url,origin)
+		socketcalls(url,origin,message)
 	}
 }
