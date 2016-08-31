8b45b172aa81165707d4cc8683ac8a3b73b50b55
Failattu <kalle.sirkesalo@eficode.com>
2016-08-31 15:24:35 +0300

    Argumented the client

--- src/client/client.go
+++ src/client/client.go
@@ -4,21 +4,26 @@ import (
 	"log"
 	"fmt"
     "golang.org/x/net/websocket"
+	"os"
 )
 func main() {
-	origin := "http://localhost/"
-	url := "ws://localhost:12345/echo"
-	ws, err := websocket.Dial(url, "", origin)
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) > 2 {
+		origin := "http://localhost/"
+		baseurl := os.Args[1]
+		baseport := os.Args[2]
+		url := "ws://"+ baseurl+":"+baseport+"/echo"
+		ws, err := websocket.Dial(url, "", origin)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := ws.Write([]byte("Red Hat  and lots of love for containers \n")); err != nil {
+			log.Fatal(err)
+		}
+		var msg = make([]byte, 512)
+		var n int
+		if n, err = ws.Read(msg); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Received: %s.\n", msg[:n])
 	}
-	if _, err := ws.Write([]byte("Red Hat  and lots of love for containers \n")); err != nil {
-		log.Fatal(err)
-	}
-	var msg = make([]byte, 512)
-	var n int
-	if n, err = ws.Read(msg); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Received: %s.\n", msg[:n])
 }
