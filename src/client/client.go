e562df2d8d2af3aaab023f3c8e6ab856d350af05
Failattu <kalle.sirkesalo@eficode.com>
2016-08-31 15:32:11 +0300

    Refactored the websocket to it's own function

--- src/client/client.go
+++ src/client/client.go
@@ -6,24 +6,27 @@ import (
     "golang.org/x/net/websocket"
 	"os"
 )
+func socketcalls(remoteurl string, originurl string){
+	ws, err := websocket.Dial(remoteurl, "", originurl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := ws.Write([]byte("Red Hat  and lots of love for containers \n")); err != nil {
+		log.Fatal(err)
+	}
+	var msg = make([]byte, 512)
+	var n int
+	if n, err = ws.Read(msg); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Received: %s.\n", msg[:n])
+}
 func main() {
 	if len(os.Args) > 2 {
 		origin := "http://localhost/"
 		baseurl := os.Args[1]
 		baseport := os.Args[2]
 		url := "ws://"+ baseurl+":"+baseport+"/echo"
-		ws, err := websocket.Dial(url, "", origin)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if _, err := ws.Write([]byte("Red Hat  and lots of love for containers \n")); err != nil {
-			log.Fatal(err)
-		}
-		var msg = make([]byte, 512)
-		var n int
-		if n, err = ws.Read(msg); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Received: %s.\n", msg[:n])
+		socketcalls(url,origin)
 	}
 }
